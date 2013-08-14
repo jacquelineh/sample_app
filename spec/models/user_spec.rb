8de58d32a27bba66c755ad56e146e59f48e8e6af
Jacqueline Han <jacquelineh@me.com>
2013-08-14 13:47:00 -0400

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe User do
 
-	before do
+  before do
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
-  	end
+  end
 
   subject { @user }
 
@@ -16,25 +16,39 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+  it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
   it { should be_valid }
+  it { should_not be_admin }
 
-	describe "when name is not present" do
-    	before { @user.name = " " }
-    	it { should_not be_valid }
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
     end
 
-	describe "when email is not present" do
-    	before { @user.email = " " }
-    	it { should_not be_valid }
-	end 
+    it { should be_admin }
+  end
 
-	describe "when name is too long" do
-    	before { @user.name = "a" * 51 }
-    	it { should_not be_valid }
-	end
+  describe "when name is not present" do
+      before { @user.name = " " }
+      it { should_not be_valid }
+    end
 
-	describe "when email format is invalid" do
+  describe "when email is not present" do
+      before { @user.email = " " }
+      it { should_not be_valid }
+  end 
+
+  describe "when name is too long" do
+      before { @user.name = "a" * 51 }
+      it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -63,7 +77,7 @@ describe User do
       end
 
       it { should_not be_valid }
-  	end
+    end
 
   describe "email address with mixed case" do
     let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
@@ -86,7 +100,7 @@ describe User do
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
-  	end
+    end
 
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
@@ -107,10 +121,44 @@ describe User do
     it { should_not eq user_for_invalid_password }
     specify { expect(user_for_invalid_password).to be_false }
     end
+  end
 
   describe "remember token" do
-  before { @user.save }
-  its(:remember_token) { should_not be_blank }
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
- end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.to_a
+      @user.destroy
+      expect(microposts).not_to be_empty
+      microposts.each do |micropost|
+        expect(Micropost.where(id: micropost.id)).to be_empty
+      end
+    end
+    
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
+  end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,23 +4,23 @@ describe "Authentication" do
 
   subject { page }
 
-	describe "signin" do
+  describe "signin" do
 
-      before { visit signin_path }
+    before { visit signin_path }
 
-	  	describe "with invalid information" do
-      	before { click_button "Sign in" }
+    describe "with invalid information" do
+      before { click_button "Sign in" }
 
-      	it { should have_title('Sign in') }
-      	it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 
-      	describe "after visiting another page" do
-        	before { click_link "Home" }
-        	it { should_not have_selector('div.alert.alert-error') }
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
       end
     end
 
-	describe "with valid information" do
+    describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in user }
 
@@ -31,25 +31,26 @@ describe "Authentication" do
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
-     describe "followed by signout" do
+      describe "followed by signout" do
         before { click_link "Sign out" }
-         it { should have_link('Sign in') }
+        it { should have_link('Sign in') }
+      end
     end
-   end
   end
 
   describe "authorization" do
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
-		
-	  it { should_not have_link('Users',       href: users_path) }
+
+      it { should_not have_link('Users',       href: users_path) }
       it { should_not have_link('Profile',     href: user_path(user)) }
       it { should_not have_link('Settings',    href: edit_user_path(user)) }
       it { should_not have_link('Sign out',    href: signout_path) }
+      it { should_not have_link('Sign in',     href: signin_path) }
 
       describe "when attempting to visit a protected page" do
-         before do
+        before do
           visit edit_user_path(user)
           fill_in "Email",    with: user.email
           fill_in "Password", with: user.password
@@ -61,8 +62,8 @@ describe "Authentication" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
-
-        describe "when signing in again" do
+          
+          describe "when signing in again" do
             before do
               delete signout_path
               visit signin_path
@@ -90,25 +91,26 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
 
-       describe "visiting the user index" do
+        describe "visiting the user index" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end
       end
-	end
+      
+      describe "in the Microposts controller" do
 
-	describe "as non-admin user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:non_admin) { FactoryGirl.create(:user) }
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
 
-      before { sign_in non_admin, no_capybara: true }
-
-      describe "submitting a DELETE request to the Users#destroy action" do
-        before { delete user_path(user) }
-        specify { expect(response).to redirect_to(root_url) }
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
       end
     end
-    
+
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
@@ -121,6 +123,18 @@ describe "Authentication" do
 
       describe "submitting a PATCH request to the Users#update action" do
         before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+   
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin, no_capybara: true }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
         specify { expect(response).to redirect_to(root_url) }
       end
     end
